c9d42bbfd739d531f840102e9a99aa9d95d7d568
PRASHANTSWAROOP001 <mrprashantswaroop@gmail.com>
2025-12-22 20:30:50 +0530

    services that include business logic.

--- app/src/main/java/org/example/password/service/Service.java
+++ app/src/main/java/org/example/password/service/Service.java
@@ -1,24 +1,29 @@
 package org.example.password.service;
 
+import org.example.Repo.PasswordRepository;
 import org.example.password.model.Password;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 public class Service {
+    private final PasswordRepository passwordRepo;
     private final List<Password> passwords;
 
 
-    public Service(List<Password> passwords) {
-        this.passwords = passwords;
+    public Service(PasswordRepository passwordRepo) {
+        this.passwordRepo = passwordRepo;
+        this.passwords = new ArrayList<>(passwordRepo.getAll());
     }
 
-    public void savePasswordList(String note, String username, String password){
+    public void savePasswordList(String username, String password, String notes, String website) {
         Password p = new Password();
         p.setUsername(username);
         p.setPassword(password);
-        p.setNotes(note);
+        p.setNotes(notes);
+        p.setWebsite(website);
         p.setCreatedAt(LocalDateTime.now());
         p.setId(UUID.randomUUID().toString());
         this.passwords.add(p);
@@ -33,7 +38,7 @@ public class Service {
         throw new RuntimeException("Password with id " + id + " not found");
     }
 
-    public void DeletePasswordById(String id){
+    public void deletePasswordById(String id){
         for (int i = 0; i<this.passwords.size(); i++){
             if(this.passwords.get(i).getId().equals(id)){
                 Password p = this.passwords.get(i);
@@ -49,14 +54,6 @@ public class Service {
         return List.copyOf(this.passwords);
     }
 
-//    public String displayPasswords(Password p){
-//        if(p.isDeleted()){
-//            return "";
-//        }
-//        else{
-//            return String.format("Id: %s |Username: %s | Password: %s| Created At: %s ",p.getId(), p.getUsername(), p.getPassword(), p.getCreatedAt());
-//        }
-//    }
 
     public void updatePassword(String id, String password){
          Password p = getPasswordById(id);
@@ -67,6 +64,10 @@ public class Service {
     public void updateNote(String id, String note){
         Password p = getPasswordById(id);
         p.setNotes(note);
+    }
+
+    public void save(){
+        passwordRepo.saveAll(this.passwords);
     }
 
 }
